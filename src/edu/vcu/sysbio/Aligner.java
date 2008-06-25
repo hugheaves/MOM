26032ae2d58fc3cbd4b57994ebebe07d3b8f99b9
hugh <hugh>
2008-06-25 19:05:56 +0000

    Performance optimizations

--- src/edu/vcu/sysbio/Aligner.java
+++ src/edu/vcu/sysbio/Aligner.java
@@ -1,60 +1,70 @@
 /*
  * $Log: Aligner.java,v $
- * Revision 1.1  2008/05/08 18:50:08  hugh
+ * Revision 1.2  2008/06/25 19:05:56  hugh
+ * Performance optimizations
+ *
+ * Revision 1.1  2008-05-08 18:50:08  hugh
  * Updated.
  *
  */
 
 package edu.vcu.sysbio;
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.objects.ObjectArraySet;
 import it.unimi.dsi.fastutil.objects.ObjectSet;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Aligner {
-	protected final int mismatchOffsets[];
-	protected final int queryLength;
-	protected final int minMatchLength;
-	protected final int maxMismatches;
-	protected final byte[] genome;
+	protected int mismatchOffsets[];
+	// protected final int queryLength;
+	// protected final int maxMismatches;
+	protected final byte[] reference;
 	protected final byte[] queries;
 	protected final byte[] matchCounts;
 	protected final int fileNum;
-	// protected final Int2ObjectMap<ObjectSet<Match>> results;
-	// protected final Set<Match> results;
 	protected final ObjectSet<Match> results;
+	protected final int[] alreadyCompared;
 
-	public Aligner(byte[] genome, byte[] queries, int queryLength,
-			int minMatchLength, int maxMismatches, int fileNum,
-			ObjectSet<Match> results, byte[] matchCounts) {
-		this.mismatchOffsets = new int[queryLength];
-		this.queryLength = queryLength;
-		this.minMatchLength = minMatchLength;
-		this.maxMismatches = maxMismatches;
-		this.genome = genome;
+	public Aligner(byte[] reference, byte[] queries, int fileNum,
+			ObjectSet<Match> results, byte[] matchCounts, int[] alreadyCompared) {
+		this.mismatchOffsets = new int[ProgramParameters.queryLength];
+		// this.queryLength = ProgramParameters.queryLength;
+		// this.maxMismatches = ProgramParameters.maxMismatches;
+		this.reference = reference;
 		this.queries = queries;
 		this.fileNum = fileNum;
 		this.results = results;
 		this.matchCounts = matchCounts;
+		this.alreadyCompared = alreadyCompared;
 	}
 
 	/**
-	 * Find the longest matching substring between the genome and query with "k"
-	 * mismatches and "q" minimum match size
+	 * Find the longest matching substring between the reference and query with
+	 * "k" mismatches and "q" minimum match size
 	 * 
-	 * @param genomePosition
+	 * @param referencePosition
 	 * @param queryPosition
 	 * @param forwardDirection
 	 */
-	public final void doAlignment(int genomePosition, int queryPosition) {
-		int compareOffset = -(queryPosition % queryLength);
+	public final void doAlignment(int referencePosition, int queryPosition) {
+		final int queryNum = queryPosition / ProgramParameters.queryLength;
+
+		if ((ProgramParameters.maxMatchesPerQuery != -1)
+				&& (matchCounts[queryNum] >= ProgramParameters.maxMatchesPerQuery)) {
+			return;
+		}
+
+		final int compareOffset = -(queryPosition % ProgramParameters.queryLength);
+
+		if (alreadyCompared != null) {
+			if (alreadyCompared[referencePosition + compareOffset] == queryNum) {
+				return;
+			} else {
+				alreadyCompared[referencePosition + compareOffset] = queryNum;
+			}
+		}
 
 		int startPos = compareOffset;
 		int endPos = compareOffset;
-		int finalPos = compareOffset + queryLength;
+		final int finalPos = compareOffset + ProgramParameters.queryLength;
 
 		int longestMatchLength = 0;
 		int longestMatchPosition = startPos;
@@ -62,17 +72,15 @@ public class Aligner {
 		int longestMatchMismatchesOffset = 0;
 		int currentNumMismatches = 0;
 		int totalMismatches = 0;
-		int maxTotalMismatches = queryLength - minMatchLength + maxMismatches;
-
-		if (matchCounts[queryPosition / queryLength] >= 2) {
-			return;
-		}
+		final int maxTotalMismatches = ProgramParameters.queryLength
+				- ProgramParameters.minMatchLength
+				+ ProgramParameters.maxMismatches;
 
 		// "caterpillar" through the string to find the largest matching
 		// substring with k mismatches
 		while (endPos < finalPos) {
 
-			if (genome[genomePosition + endPos] != queries[queryPosition
+			if (reference[referencePosition + endPos] != queries[queryPosition
 					+ endPos]) {
 
 				mismatchOffsets[totalMismatches++] = endPos;
@@ -86,11 +94,11 @@ public class Aligner {
 				// increment mismatching character count
 				++currentNumMismatches;
 
-				if (currentNumMismatches > maxMismatches) {
+				if (currentNumMismatches > ProgramParameters.maxMismatches) {
 					// if we're "maxed out" on mismatches, we "eat" a mismatch
 					// from the beginning
-					startPos = mismatchOffsets[totalMismatches - maxMismatches
-							- 1] + 1;
+					startPos = mismatchOffsets[totalMismatches
+							- ProgramParameters.maxMismatches - 1] + 1;
 					--currentNumMismatches;
 				}
 			}
@@ -98,8 +106,9 @@ public class Aligner {
 			++endPos;
 
 			// check to see if we have a new "longest substring"
-			if ((endPos - startPos) > longestMatchLength
-					|| ((endPos - startPos) == longestMatchLength && currentNumMismatches < longestMatchMismatches)) {
+			if ((endPos - startPos) > longestMatchLength) {
+				// || ((endPos - startPos) == longestMatchLength &&
+				// currentNumMismatches < longestMatchMismatches)) {
 				longestMatchLength = endPos - startPos;
 				longestMatchMismatches = currentNumMismatches;
 				longestMatchPosition = startPos;
@@ -108,35 +117,15 @@ public class Aligner {
 
 		}
 
-		if (longestMatchLength >= minMatchLength) {
-			int queryPos = queryPosition + compareOffset;
-
-			// ObjectSet<Match> matchSet;
-			// synchronized (results) {
-			// matchSet = results.get(queryPos);
-			// if (matchSet == null) {
-			// matchSet = new ObjectArraySet<Match>(2);
-			// results.put(queryPos, matchSet);
-			// }
-			// }
-			//
-			// if (matchSet.size() >= 2) {
-			// return;
-			// }
-
-			Match match = new Match(fileNum, genomePosition + compareOffset,
+		if (longestMatchLength >= ProgramParameters.minMatchLength) {
+			Match match = new Match(fileNum, referencePosition + compareOffset,
 					queryPosition + compareOffset, longestMatchPosition
 							- compareOffset, longestMatchPosition
 							- compareOffset + longestMatchLength - 1,
 					longestMatchMismatches, null);
 
 			if (results.add(match)) {
-				++matchCounts[queryPosition / queryLength];
-				// boolean added;
-				// synchronized (matchSet) {
-				// added = matchSet.add(match);
-				// }
-				// if (added && longestMatchMismatches > 0) {
+				++matchCounts[queryPosition / ProgramParameters.queryLength];
 				if (longestMatchMismatches > 0) {
 
 					byte[] mismatches = new byte[longestMatchMismatches * 2];
@@ -145,7 +134,7 @@ public class Aligner {
 						int offset = mismatchOffsets[longestMatchMismatchesOffset
 								- longestMatchMismatches + i];
 						mismatches[j++] = (byte) (offset - compareOffset + 1);
-						mismatches[j++] = genome[genomePosition + offset];
+						mismatches[j++] = reference[referencePosition + offset];
 					}
 
 					match.mismatches = mismatches;
@@ -153,6 +142,11 @@ public class Aligner {
 			}
 
 		}
+	}
 
+	public Object clone() throws CloneNotSupportedException {
+		Aligner newAligner = (Aligner) super.clone();
+		newAligner.mismatchOffsets = new int[ProgramParameters.queryLength];
+		return newAligner;
 	}
 }
